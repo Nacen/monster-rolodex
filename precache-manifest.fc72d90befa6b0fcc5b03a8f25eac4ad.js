fb8609ca9c11b1ca8ac493b29e68f56e97ccac5a
Vincent <overlordnacen@gmail.com>
2019-10-30 22:41:33 +0800

    Updates

---
+++ precache-manifest.fc72d90befa6b0fcc5b03a8f25eac4ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85443179b7857401c119bad66d81a7f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1b5a6614728d3560c07d",
+    "url": "/monster-rolodex/static/css/main.4a9a6e2d.chunk.css"
+  },
+  {
+    "revision": "a6e7282b7dd326a54618",
+    "url": "/monster-rolodex/static/js/2.2976affa.chunk.js"
+  },
+  {
+    "revision": "1b5a6614728d3560c07d",
+    "url": "/monster-rolodex/static/js/main.c1c0cf89.chunk.js"
+  },
+  {
+    "revision": "e5540c1030566d31d8b0",
+    "url": "/monster-rolodex/static/js/runtime-main.bd5b8595.js"
+  }
+]);
